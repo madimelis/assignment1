4203158043d7de31d87b5e015886a1a4d40dc155
Madi Melis <250270@astanait.edu.kz>
2026-03-24 17:54:53 +0500

    First task

---
+++ src/exOne.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+public class exOne {
+    public static void printDigits(int n) {
+        if (n < 10) {System.out.println(n);}
+        else {
+            printDigits(n / 10);
+            System.out.println(n % 10);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        printDigits(number);
+    }
+}
