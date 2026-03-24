cc4e70c8cbbad0e56295354685cc872a7456cc9f
Madi Melis <250270@astanait.edu.kz>
2026-03-24 20:06:12 +0500

    Fourth task

---
+++ src/exFive.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class exFive {
+    public static int fib(int n) {
+        if (n == 0) {return 0;}
+        else if (n == 1) {return 1;}
+        else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+        System.out.println(fib(number));
+    }
+}
