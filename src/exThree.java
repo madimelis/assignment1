7098f1d1865220f93f4df2aa3f0c9b1b53326068
Madi Melis <250270@astanait.edu.kz>
2026-03-24 19:44:03 +0500

    Third task

---
+++ src/exThree.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class exThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isPrime(num, 2)) {
+            System.out.println("Prime");
+        }
+        else {System.out.println("Composite");}
+        sc.close();
+    }
+
+    public static boolean isPrime(int n, int d) {
+        if (n <= 1) {return false;}
+        else if (d * d > n) {
+            return true;
+        }
+        else if (n % d == 0) {
+            return false;
+        }
+        else {
+            return isPrime(n, d + 1);
+        }
+    }
+}
