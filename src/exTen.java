3f4f1d2f07f97ae373598ad9e7a36ac2993d7042
Madi Melis <250270@astanait.edu.kz>
2026-03-24 21:32:17 +0500

    Tenth task

---
+++ src/exTen.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exTen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println(gcd(a, b));
+        sc.close();
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        else {
+            return gcd(b, a % b);
+        }
+    }
+}
